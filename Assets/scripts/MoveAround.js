a457cd5f01b318487bdf7fff7b471b77f9c16d97
Ryan Morash <ryan@morash.me>
2014-05-07 14:23:31 -0400

    Video 4
    Added initial fireball control scripting

--- Assets/scripts/MoveAround.js
+++ Assets/scripts/MoveAround.js
@@ -1,5 +1,6 @@
 ﻿var speed = 3.0;
 var rotateSpeed = 3.0;
+var bulletPrefab:Transform;
 
 function Update () {
 	var controller : CharacterController = GetComponent(CharacterController);
@@ -11,4 +12,9 @@ function Update () {
 	var forward = transform.TransformDirection(Vector3.forward);
 	var curSpeed = speed * Input.GetAxis ("Vertical");
 	controller.SimpleMove(forward * curSpeed);
+	
+	if(Input.GetButtonDown("Fire")) {
+		var bullet = Instantiate(bulletPrefab, GameObject.Find("spawnPoint").transform.position, Quaternion.identity);
+		bullet.rigidbody.AddForce(transform.forward * 2000);
+	}
 }
