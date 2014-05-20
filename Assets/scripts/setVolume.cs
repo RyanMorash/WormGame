dc4c7d32dbe95ac7cfc0737c0c8712a6b5312daa
Ryan Morash <ryan@morash.me>
2014-05-20 14:29:52 -0400

    Adding menu scripting
    Started volume options scripting

---
+++ Assets/scripts/setVolume.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setVolume : MonoBehaviour {
+
+	private UISlider _slider;
+	public static float musicVol = 1;
+
+	// Use this for initialization
+	void Start () {
+		_slider = gameObject.GetComponent<UISlider>();
+		_slider.sliderValue = musicVol;
+		_slider.onValueChange += OnValueChange;
+	}
+	
+	// Update is called once per frame
+	void OnValueChange(float val) {
+		musicVol = val;
+		print(val);
+	}
+}
